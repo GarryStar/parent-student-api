b30ef4a75212eacf21017474d2961dc8c39a80f5
Gary <garyczstar@gmail.com>
2026-04-17 12:35:11 +0200

    add users

--- app/main.py
+++ app/main.py
@@ -3,7 +3,7 @@ from fastapi.security import OAuth2PasswordRequestForm
 from sqlalchemy.orm import Session
 
 from app.database import Base, engine, get_db, SessionLocal
-from app import models, auth
+from app import models, auth, schemas
 
 app = FastAPI()
 
@@ -64,3 +64,29 @@ def login(
 @app.get("/me")
 def read_me(current_user: dict = Depends(auth.get_current_user)):
     return current_user
+
+@app.post("/users", response_model=schemas.UserRead)
+def create_user(
+    user_data: schemas.UserCreate,
+    current_user=Depends(auth.require_admin),
+    db: Session = Depends(get_db)
+):
+    existing_user = db.query(models.User).filter(models.User.username == user_data.username).first()
+
+    if existing_user:
+        raise HTTPException(status_code=400, detail="Username už existuje")
+
+    if user_data.role not in ["admin", "parent"]:
+        raise HTTPException(status_code=400, detail="Role musí být admin nebo parent")
+
+    new_user = models.User(
+        username=user_data.username,
+        password_hash=auth.hash_password(user_data.password),
+        role=user_data.role
+    )
+
+    db.add(new_user)
+    db.commit()
+    db.refresh(new_user)
+
+    return new_user
